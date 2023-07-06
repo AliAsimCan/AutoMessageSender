f7e67f28b1c7d73061376f0b8b169dc1fae356dd
Ali Asım CAN <131978084+AliAsimCan@users.noreply.github.com>
2023-07-06 22:54:23 +0300

    main.py
    My first amateur project

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 def mesaj():
-    pyautogui.write("I'm trying to python code.")
+    pyautogui.write("I'm trying to python code.") #You can edit the message here
     pyautogui.press('enter')
 
 while True:
